20dbe0c1ddb25d9e293ff2acfb93a4f90f1be888
Matheus Artioli <matheus.sartioli@gmail.com>
2026-05-11 17:58:29 -0300

    Adicionando desafio 1002-area-do-circulo

--- Iniciante/1002-area-do-circulo.java
+++ Iniciante/1002-area-do-circulo.java
@@ -1,9 +1,6 @@
 import java.util.Scanner;   
 
-
-
-
-public class AreaDoCirculo {
+public class 1002-area-do-circulo {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         double pi = 3.14159;
