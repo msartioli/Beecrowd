e1e42e6967b6ec9abf39ed7dadb8fd06f3fc65d7
Matheus Artioli <matheus.sartioli@gmail.com>
2026-05-12 10:01:42 -0300

    +1 Desafio concluido - 1003

---
+++ Iniciante/1003-soma-simples.java
@@ -0,0 +1,9 @@
+public class 1003-soma-simples {
+    public static void main (String[] args()){
+      Scanner scanner = new Scanner(System.in);
+      int A = scanner.nextInt();
+      int B = scanner.nextInt();
+        int SOMA = A + B;
+        System.out.println("SOMA = " + SOMA);
+    }
+}
