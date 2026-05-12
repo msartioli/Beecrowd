a1c7aa09e589443c9beff70cd3a7568ecd1d008e
Matheus Artioli <matheus.sartioli@gmail.com>
2026-05-12 10:06:13 -0300

    +1 Desafio Concluido - 1004

---
+++ Iniciante/1004-produto-simples.java
@@ -0,0 +1,8 @@
+public class 1004-produto-simples {
+    public static void main (String[] args){
+      Scanner scanner = new Scanner (System.in);
+        int A = scanner.nextInt(); 
+        int B = scanner.nextInt();
+        int PROD = A * B;
+    }
+}
