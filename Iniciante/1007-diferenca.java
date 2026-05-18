734267beb00d14d2721da29d53cc9442c37558cb
Matheus Artioli <matheus.sartioli@gmail.com>
2026-05-18 08:35:34 -0300

    +1 Desafio concluido - 1007

---
+++ Iniciante/1007-diferenca.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+/**
+ * IMPORTANT: 
+ *      O nome da classe deve ser "Main" para que a sua solução execute
+ *      Class name must be "Main" for your solution to execute
+ *      El nombre de la clase debe ser "Main" para que su solución ejecutar
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+ 
+        Scanner scanner = new Scanner (System.in);
+        int A = scanner.nextInt();
+        int B = scanner.nextInt();
+        int c = scanner.nextInt();
+        int D = scanner.nextInt();
+        int DIFERENCA = (A * B) - (c * D);
+        System.out.println("DIFERENCA = " + DIFERENCA);
+    }
+ 
+}
